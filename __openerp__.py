6dd4a1c176ee9d34e0d706176580427ba61dd099
leti12 <leticia.gonzalez@quadit.mx>
2019-05-16 12:43:10 -0500

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'custom_website',
+    'version': '9.0.1.0.0',
+    'depends': [
+    ],
+    'author': 'Quadit, S.A. de C.V.',
+    'description': 'custom website',
+    'website': 'https://www.quadit.mx',
+    'data': [
+    'views/templates.xml',
+    ],
+    'demo': [],
+    'installable': True
+}
